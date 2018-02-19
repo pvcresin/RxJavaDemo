a0c745c5c1bc4bd48396be993e16f940d3432978
pvcresin <pvcresin0730@gmail.com>
2018-02-19 16:17:24 +0900

    added compositeDisposable

--- app/src/main/java/com/pvcresin/rxjavademo/MainActivity.kt
+++ app/src/main/java/com/pvcresin/rxjavademo/MainActivity.kt
@@ -3,26 +3,49 @@ package com.pvcresin.rxjavademo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import io.reactivex.Observable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        val TAG = "GitHub"
-    }
+    val TAG = "GitHub"
+    // task manager
+    val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // operate list
+        val listTask = Observable
+                .just(
+                        Arrays.asList("Alice", "Bob", "Charlie"),
+                        Arrays.asList("Dave", "Ellen", "Frank")
+                )
+                .subscribeOn(Schedulers.io())
+                .flatMap { list -> Observable.fromIterable<String>(list) }  // flatten
+                .filter { name -> name.length > 4 }                         // filter by length
+                .map { name -> "$name(length ${name.length})" }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { text -> Log.d(TAG, "text: $text") },
+                        { t: Throwable? -> t?.printStackTrace() },
+                        { Log.d(TAG, "complete") }
+                )
+
+        compositeDisposable.add(listTask)
+
+        // operate api
         val retrofit = Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
@@ -31,20 +54,20 @@ class MainActivity : AppCompatActivity() {
 
         val service = retrofit.create(GitHubService::class.java)
 
-        service.listRepos("pvcresin")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
+        val singleTask = service.listRepos("pvcresin")
+                .subscribeOn(Schedulers.io())               // async process (api)
+                .observeOn(AndroidSchedulers.mainThread())  // get result at UI thread
                 .subscribe { list ->
-                    list.forEach { repo ->
-                        Log.d(TAG, repo.toString())
-                    }
+                    list.filterIndexed { index, repo -> index < 5 }
+                            .forEach { repo -> Log.d(TAG, repo.toString()) }
                 }
-    }
-}
 
-fun doSomethingToList(list: List<Repo>?) {
-    list?.forEach {
-        Log.d(MainActivity.TAG, it.toString())
+        compositeDisposable.add(singleTask)
+    }
+
+    override fun onDestroy() {
+        compositeDisposable.clear()     // dispose all task
+        super.onDestroy()
     }
 }
 
